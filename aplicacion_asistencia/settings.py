83bb5a8373aa801f1af55ab42ce6836042313ccd
Kossy Emmanuel <kossyemmanuel@Kossys-MacBook-Pro.local>
2025-04-28 15:50:55 -0600

    Static fixes 3

--- aplicacion_asistencia/settings.py
+++ aplicacion_asistencia/settings.py
@@ -39,12 +39,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'whitenoise.runserver_nostatic',
     "registros",
     "scanner",
     "estadisticas"
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
