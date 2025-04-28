ce00817075dd66e2b69ee429f874d5993fe5909e
Kossy Emmanuel <kossyemmanuel@Kossys-MacBook-Pro.local>
2025-04-28 17:45:55 -0600

    media files not loading

--- aplicacion_asistencia/urls.py
+++ aplicacion_asistencia/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("registros/", include("registros.urls")),
     path("estadisticas/", include("estadisticas.urls"))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if not settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
